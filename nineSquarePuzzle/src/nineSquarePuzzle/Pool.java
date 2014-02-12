8d629a5224dd94716f62a9637b6b9721b363ec8b
Paul Cottin <paulcottin@gmail.com>
2014-02-12 21:45:16 +0100

    lecture des fichiers pour le chargement des pièces

--- nineSquarePuzzle/src/nineSquarePuzzle/Pool.java
+++ nineSquarePuzzle/src/nineSquarePuzzle/Pool.java
@@ -1,14 +1,94 @@
 package nineSquarePuzzle;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Pool {
 
-	public Pool(){
-		
+	private ArrayList<String> lignes = new ArrayList<String>();
+	private String titre = "Pas de titre";
+	private ArrayList<Piece> pool = new ArrayList<Piece>();
+	
+	public Pool(String path){
+		this.charge(path);
+		this.titre = lignes.get(0);
+		this.creePool();
 	}
 	
-	public File charge(String path){
-		return new File(path);
+	public void creePool(){
+		for (int i = 1; i < 10; i++) {
+			pool.add(this.creePieces(lignes.get(i)));
+		}
+	}
+	public Piece creePieces(String ligne){
+		int longueur = ligne.length();
+		String titre = ligne.substring(0, 1);
+		String chiffre1 = "", chiffre2 = "";
+		int[] tab = new int[4];
+		int cpt = 0, i = 1;
+		while (i < longueur - 1) {
+			chiffre1 = ""; chiffre2 = "";
+			if (ligne.substring(i, i+1).equals("-")) {
+				chiffre1 = "-";
+				i++;
+			}
+			else if (ligne.substring(i, i+1).equals(" ")) {i++;}
+			if(ligne.substring(i, i+1).equals("1") || ligne.substring(i, i+1).equals("2") || ligne.substring(i, i+1).equals("3") || ligne.substring(i, i+1).equals("4")){
+				chiffre2 = ligne.substring(i, i+1);
+				i++;
+			}
+			String ch = chiffre1.concat(chiffre2);
+			if (!ch.equals("")) {
+				tab[cpt] = Integer.valueOf(ch);
+				cpt++;
+			}
+		}
+		return new Piece(titre, tab);
+	}
+	
+	
+	public void charge(String path){
+		String ligne = "";
+		try {
+			File f = new File(path);
+			BufferedReader buf = new BufferedReader(new FileReader(f));
+			while ((ligne = buf.readLine()) != null){
+				lignes.add(ligne);
+			}
+			buf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<String> getLignes() {
+		return lignes;
+	}
+
+	public void setLignes(ArrayList<String> lignes) {
+		this.lignes = lignes;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		if (titre.length() != 1) {
+			System.out.println("Le nom d'une piece doit etre une lettre !");
+		}else {
+			this.titre = titre;
+		}
+	}
+
+	public ArrayList<Piece> getPool() {
+		return pool;
+	}
+
+	public void setPool(ArrayList<Piece> pool) {
+		this.pool = pool;
 	}
 }
