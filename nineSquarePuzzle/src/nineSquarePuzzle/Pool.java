8238ea3c99ff522b57c5391d7ae5996d0213a0a9
Paul Cottin <paulcottin@gmail.com>
2014-02-12 19:25:03 +0100

    Ajout des data.txt

--- nineSquarePuzzle/src/nineSquarePuzzle/Pool.java
+++ nineSquarePuzzle/src/nineSquarePuzzle/Pool.java
@@ -1,6 +1,14 @@
 package nineSquarePuzzle;
 
+import java.io.File;
+
 public class Pool {
 
-	public Pool()
+	public Pool(){
+		
+	}
+	
+	public File charge(String path){
+		return new File(path);
+	}
 }
